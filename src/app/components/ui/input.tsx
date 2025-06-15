ba9db5365fff523a1602f354e1695273a67207bd
Casper Nag <casper_nag@outlook.com>
2025-06-15 16:07:04 +0200

    vercel fix #2

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,9 +1,6 @@
 import * as React from "react"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  // Additional props can be added here if needed
-}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className = "", type, ...props }, ref) => {
--- src/app/components/ui/textarea.tsx
+++ src/app/components/ui/textarea.tsx
@@ -1,9 +1,6 @@
 import * as React from "react"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  // Additional props can be added here if needed
-}
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className = "", ...props }, ref) => {
